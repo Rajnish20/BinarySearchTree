6cd50e7260f38db066f1cf7c368dbad757d31a8d
Rajnish <38453583+Rajnish20@users.noreply.github.com>
2019-07-05 23:11:31 +0530

    Added search function

--- BST.cpp
+++ BST.cpp
@@ -13,18 +13,16 @@ BSTnode* getNewNode(int data)
 {
     BSTnode* Newnode = new BSTnode();
     Newnode -> left = Newnode -> right = NULL;
-    NewNode -> data = data;
+    Newnode -> data = data;
     return Newnode;
 }
 BSTnode* insertNode(BSTnode* root, int data)
 {
     if(root == NULL)
     {
-        root = getNewNode(int data);
+        root = getNewNode(data);
     }
-    else
-    {
-        if(data <= root -> data)
+    else if(data <= root -> data)
         {
             root -> left = insertNode(root -> left,data);
         }
@@ -34,17 +32,56 @@ BSTnode* insertNode(BSTnode* root, int data)
 
 
         }
-    }
     return root;
 
+}
+bool search(BSTnode* root, int data)
+{
+    if(root == NULL)
+    {
+        return false;
+    }
+    else
+    {
+        if(data == root -> data)
+        {
+            return true;
+        }
+        else
+        {
+            if(data <= root -> data)
+            {
+                return search(root -> left, data);
+            }
+            else
+            {
+                return search(root -> right, data);
+            }
+        }
+    }
 }
 
 
 int main()
 {
+    int n;
     BSTnode* root = NULL;
     root = insertNode(root, 12);
     root = insertNode(root, 5);
     root = insertNode(root, 20);
+    root = insertNode(root, 25);
+    root = insertNode(root, 1);
+    root = insertNode(root, 10);
+    cout<<"Enter the no to be searched";
+    cin>>n;
+    if(search(root,n) == true)
+    {
+        cout<<"Number found";
+    }
+    else
+    {
+        cout<<"Not found";
+    }
+
     return 0;
 }
