8656dab56bdc410e82a74ff97a6bc3cebdfdcb34
Rajnish <38453583+Rajnish20@users.noreply.github.com>
2019-07-05 16:49:47 +0530

    Updated BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -2,9 +2,49 @@
 
 using namespace std;
 
+struct BSTnode
+{
+    int data;
+    BSTnode* left;
+    BSTnode* right;
+
+};
+BSTnode* getNewNode(int data)
+{
+    BSTnode* Newnode = new BSTnode();
+    Newnode -> left = Newnode -> right = NULL;
+    NewNode -> data = data;
+    return Newnode;
+}
+BSTnode* insertNode(BSTnode* root, int data)
+{
+    if(root == NULL)
+    {
+        root = getNewNode(int data);
+    }
+    else
+    {
+        if(data <= root -> data)
+        {
+            root -> left = insertNode(root -> left,data);
+        }
+        else
+        {
+            root ->right = insertNode(root -> right, data);
+
+
+        }
+    }
+    return root;
+
+}
+
+
 int main()
 {
-    int n = 1/2;
-    cout<<n;
+    BSTnode* root = NULL;
+    root = insertNode(root, 12);
+    root = insertNode(root, 5);
+    root = insertNode(root, 20);
     return 0;
 }
